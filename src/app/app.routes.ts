70a5045596d2552c7981286936f6b500bd121698
Mohamed Al hassan <138093476+anonusernum667@users.noreply.github.com>
2024-08-31 04:06:44 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-import { GalleryComponent } from './gallery/gallery.component';
 import { NgModule } from '@angular/core';
 import { AboutComponent } from './about/about.component';
 import { NotFFoundComponent } from './not-ffound/not-ffound.component';
@@ -10,7 +9,6 @@ export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'portfolio', component: PortfolioComponent},
-  {path: 'gallery', component: GalleryComponent},
   {path: 'skills', component:AboutComponent},
   {path:'**',component:NotFFoundComponent }
 ];
